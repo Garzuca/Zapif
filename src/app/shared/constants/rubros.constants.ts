43c0a1636a477abd93c00e47c92274885f3dc995
Axel Facundo <maiggaslima@gmail.com>
2026-08-05 10:37:19 -0400

    ajuste de rubros

--- src/app/shared/constants/rubros.constants.ts
+++ src/app/shared/constants/rubros.constants.ts
@@ -1,10 +1,8 @@
 export const RUBROS_OPCIONES = [
-    { label: 'Gastronomía y Alimentos', value: 'Gastronomia' },
     { label: 'Tecnología y Software', value: 'Tecnologia' },
-    { label: 'Artesanía y Cultura', value: 'Artesania' },
+    { label: 'Gastronomía y Alimentos', value: 'Gastronomia' },
+    { label: 'Textil y Moda', value: 'Moda' },
+    { label: 'Construcción y Hogar', value: 'Vivienda' },
     { label: 'Servicios e Industria', value: 'Servicios' },
-    { label: 'Automotriz', value: 'Automotriz' },
-    { label: 'Construcción y Hogar', value: 'Construccion' },
-    { label: 'Textil y Moda', value: 'Textil' },
     { label: 'Otros Rubros', value: 'Otros' }
 ];
