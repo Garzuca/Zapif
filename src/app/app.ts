80de49f705a02bd72ccca8948a1496028813b845
Axel Facundo <maiggaslima@gmail.com>
2026-08-14 13:11:23 -0400

    ajustes visules sobre el apartado de nosotros

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,73 +1,42 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { DashboardMetricas } from './components/dashboard-metricas/dashboard-metricas';
-import { TableroEventosComponent } from './components/tablero-eventos/tablero-eventos';
-import { PatrocinadoresPremiun } from './components/patrocinadores-premiun/patrocinadores-premiun';
+import { RouterOutlet } from '@angular/router';
 import { NavbarFipazComponent } from './shared/navbar-fipaz/navbar-fipaz';
-import { HeroFipaz } from './components/hero-fipaz/hero-fipaz';
+import { FooterFipazComponent } from './components/footer-fipaz/footer-fipaz';
 import { ExpositoresWizardComponent } from './components/expositores-wizard/expositores-wizard';
 import { VisitantesFipazComponent } from './components/visitantes-fipaz/visitantes-fipaz';
-import { FooterFipazComponent } from './components/footer-fipaz/footer-fipaz';
-import { GaleriaHistoricaComponent } from './components/galeria-historica/galeria-historica';
-import { ContadorFipazComponent } from "./components/contador-fipaz/contador-fipaz";
-import { SeccionInstitucionalComponent } from './components/seccion-institucional/seccion-institucional';
-import { BlogsFipaz } from './components/blogs-fipaz/blogs.fipaz';
-import { ContactosFipaz } from './components/contactos-fipaz/contactos-fipaz';
-import { GestionAuspiciadores } from './components/gestion-auspiciadores/gestion-auspiciadores';
-import { SeccionTikTokComponent } from "./components/seccion-tiktok/seccion-tiktok";
-import { SeccionTestimoniosComponent } from "./components/testimonios/testimonios";
-import { VideoYoutube } from "./components/video-youtube/video-youtube";
-import { RouterOutlet } from "@angular/router";
-import { NosotrosFipaz } from './components/nosotros-fipaz/nosotros-fipaz';
-
+import { VideoYoutube } from './components/video-youtube/video-youtube';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    CommonModule,
-    DashboardMetricas,
-    TableroEventosComponent,
-    PatrocinadoresPremiun,
+    RouterOutlet,
     NavbarFipazComponent,
-    HeroFipaz,
+    FooterFipazComponent,
     ExpositoresWizardComponent,
     VisitantesFipazComponent,
-    FooterFipazComponent,
-    GaleriaHistoricaComponent,
-    ContadorFipazComponent,
-    SeccionInstitucionalComponent,
-    BlogsFipaz,
-    ContactosFipaz,
-    GestionAuspiciadores,
-    SeccionTikTokComponent,
-    SeccionTestimoniosComponent,
-    VideoYoutube,
-    RouterOutlet,
-    NosotrosFipaz
+    VideoYoutube
   ],
   templateUrl: './app.html',
-  styleUrls: ['./app.css']
+  styleUrl: './app.css'
 })
 export class AppComponent {
-  title = 'fipaz-app';
+  modalExpositoresAbierto = false;
+  modalVisitantesAbierto = false;
 
-  modalExpositoresAbierto: boolean = false;
-  modalVisitantesAbierto: boolean = false;
-
-  abrirModalExpositores(): void {
+  abrirModalExpositores() {
     this.modalExpositoresAbierto = true;
   }
 
-  cerrarModalExpositores(): void {
+  cerrarModalExpositores() {
     this.modalExpositoresAbierto = false;
   }
 
-  abrirModalVisitantes(): void {
+  abrirModalVisitantes() {
     this.modalVisitantesAbierto = true;
   }
 
-  cerrarModalVisitantes(): void {
+  cerrarModalVisitantes() {
     this.modalVisitantesAbierto = false;
   }
 }
---
+++ src/app/pages/home/home.ts
@@ -0,0 +1,50 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { DashboardMetricas } from '../../components/dashboard-metricas/dashboard-metricas';
+import { TableroEventosComponent } from '../../components/tablero-eventos/tablero-eventos';
+import { PatrocinadoresPremiun } from '../../components/patrocinadores-premiun/patrocinadores-premiun';
+import { HeroFipaz } from '../../components/hero-fipaz/hero-fipaz';
+import { ExpositoresWizardComponent } from '../../components/expositores-wizard/expositores-wizard';
+import { VisitantesFipazComponent } from '../../components/visitantes-fipaz/visitantes-fipaz';
+import { FooterFipazComponent } from '../../components/footer-fipaz/footer-fipaz';
+import { GaleriaHistoricaComponent } from '../../components/galeria-historica/galeria-historica';
+import { ContadorFipazComponent } from "../../components/contador-fipaz/contador-fipaz";
+import { SeccionInstitucionalComponent } from '../../components/seccion-institucional/seccion-institucional';
+import { BlogsFipaz } from '../../components/blogs-fipaz/blogs.fipaz';
+import { ContactosFipaz } from '../../components/contactos-fipaz/contactos-fipaz';
+import { GestionAuspiciadores } from '../../components/gestion-auspiciadores/gestion-auspiciadores';
+import { SeccionTikTokComponent } from "../../components/seccion-tiktok/seccion-tiktok";
+import { SeccionTestimoniosComponent } from "../../components/testimonios/testimonios";
+import { VideoYoutube } from "../../components/video-youtube/video-youtube";
+import { NosotrosFipaz } from '../../components/nosotros-fipaz/nosotros-fipaz';
+
+@Component({
+  selector: 'app-home',
+  imports: [
+    DashboardMetricas,
+    TableroEventosComponent,
+    PatrocinadoresPremiun,
+    HeroFipaz,
+    ExpositoresWizardComponent,
+    VisitantesFipazComponent,
+    FooterFipazComponent,
+    GaleriaHistoricaComponent,
+    ContadorFipazComponent,
+    SeccionInstitucionalComponent,
+    BlogsFipaz,
+    ContactosFipaz,
+    GestionAuspiciadores,
+    SeccionTikTokComponent,
+    SeccionTestimoniosComponent,
+    VideoYoutube,
+    NosotrosFipaz
+  ],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {
+  @Output() OnQuieroExponerClick = new EventEmitter<void>();
+
+  abrirModalExpositores() {
+    this.OnQuieroExponerClick.emit();
+  }
+}
