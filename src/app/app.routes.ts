f230e4961fed683915b61c15b098351a8948fc4a
Axel Facundo <maiggaslima@gmail.com>
2026-08-17 11:37:28 -0400

    corrigiendo fallo en las rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,11 @@
 import { Routes } from '@angular/router';
 export const routes: Routes = [
     {
+        path: '',
+        loadComponent: () => import('./pages/home/home').then(m => m.Home),
+        title: 'FIPAZ 2026 - 25 años de historia'
+    },
+    {
         path: 'nosotros',
         loadComponent: () => import('./components/nosotros-fipaz/nosotros-fipaz').then(m => m.NosotrosFipaz),
         title: 'Sobre Nosotros - FIPAZ'
